616fc5c26d55144c1c1e49905bc87744be465db4
AP Java Period 5 <apjavaperiod5@ac1mwk17bjc9h.78b505.nycboe.org>
2022-10-14 12:12:10 -0400

    Now

--- 06-Username/UserName.java
+++ 06-Username/UserName.java
@@ -4,12 +4,11 @@ public class UserName {
   public static void main(String[] args) {
     // create Scanner object
     Scanner s = new Scanner(System.in);
+    // get String input from user
     String teacher =("teacher");
     String student =("student");
     System.out.println("Are you a teacher or student: ");
-    String user =s.nextLine();
-    
-    // get String input from user
+    String user =s.nextLine(); 
     System.out.print("Enter your first name: ");
     String firstName = s.nextLine();
     System.out.print("Enter your last name: ");
@@ -17,10 +16,10 @@ public class UserName {
     System.out.print("Enter your favorite number: ");
     String num =s.nextLine();
     // test output
-    if (user.equals("student")){
+    if (user.equals("student")){//If the user types student it will set up the gmail in the student format
      System.out.println("The school gmail you made is " + (firstName) + initial(lastName) + (num)+"@nycstudents.net");
     }
-     else 
+     else //Other wise if user typs teacher it will set up the gmail in the teacher format 
       System.out.println("The school gmail you made is " + initial(firstName)+(lastName) + (num)+"@schools.nyc.gov");
     s.close();
     
